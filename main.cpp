4c9e689bd99ba3bd15e818c153f5e4c3a8deab2d
ethan <ethan.lee@frodobots.com>
2024-11-13 12:09:36 +0800

    example for read/write.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 #include "SCServo.h"
 
 volatile static bool b_exit = false;
@@ -23,6 +24,7 @@ uint8_t ACC[6] = {50, 50, 50, 50, 50, 50};
 
 int main(int argc, const char *argv[]) 
 {
+	uint8_t rxPacket[4];
 	int ret = 0;
 	if (argc < 2) {
 		printf("argc error! Please provide the serial port as an argument.\n");
@@ -40,10 +42,29 @@ int main(int argc, const char *argv[])
 
 	sm_st.SyncWritePosEx(ID, sizeof(ID), Position, Speed, ACC);
 
-	while (b_exit) {
+	sm_st.syncReadBegin(sizeof(ID), sizeof(rxPacket));
+	while (!b_exit) {
 		usleep(1000 * 1000);
+		
+		printf("sync read...\n");
+
+		sm_st.syncReadPacketTx(ID, sizeof(ID), SMS_STS_PRESENT_POSITION_L,
+				sizeof(rxPacket)); // Sync read command packet send
+
+		for (uint8_t i = 0; i < 6; i++) {
+			if (!sm_st.syncReadPacketRx(ID[i], rxPacket)) {
+				printf("ID: %d sync read error!\n", (int)ID[i]);
+				continue; // Failed to receive or decode
+			}
+
+			int16_t Positionx = sm_st.syncReadRxPacketToWrod(15); // Decode position
+			int16_t Speedx = sm_st.syncReadRxPacketToWrod(15);    // Decode speed
+			printf("Pose[%d] speed[%d]\n", Positionx, Speedx);
+		}
+
 	}
 
+	sm_st.syncReadEnd();
 	sm_st.end();
 	return ret;
 }
